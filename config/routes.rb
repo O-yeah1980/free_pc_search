2273faf6e648e0a72a97305b87d8528f19d8d30a
O-yeah1980 <1980itikyuppa@gmail.com>
2020-08-02 15:03:34 +0900

    create user-regist controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+
   root "searches#index"
   resources :searches, except: :index
 end
